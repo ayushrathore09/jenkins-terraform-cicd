33b1193666bab8313bb17f8a1393557ab3f6c2d9
ayush <ayushrathore0909@gmail.com>
2025-10-27 23:34:22 +0530

    add

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_key_pair" "test-key" {
   key_name   = "test-key"
   //public_key = file("mykey.pub")
   // will pass those keys present on jenkins server
-  public_key = file("/home/ubuntu/.ssh/id_ed25519.pub")
+  public_key = file("${path.module}/id_ed25519.pub")
 }
 
 resource "aws_security_group" "test-sg" {
