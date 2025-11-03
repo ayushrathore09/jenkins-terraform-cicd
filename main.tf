007b1a9302ba6cc00c6259e40ef3c3cde43bc071
ayush <ayushrathore0909@gmail.com>
2025-11-03 23:42:04 +0530

    add

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 resource "aws_instance" "server1" {
-    ami = "ami-0360c520857e3138f"
-    instance_type = "t2.micro"
-    key_name = aws_key_pair.test-key.id
-    vpc_security_group_ids = [aws_security_group.test-sg.id]
-    tags = {
-      Name = "test-server-1"
-    }
+  ami                    = "ami-0360c520857e3138f"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.test-key.id
+  vpc_security_group_ids = [aws_security_group.test-sg.id]
+  tags = {
+    Name = "test-server-1"
+  }
 }
 
 resource "aws_key_pair" "test-key" {
-  key_name   = "testing-key"
+  key_name = "testing-key"
   //public_key = file("mykey.pub")
   // will pass those keys present on jenkins server
   public_key = file("${path.module}/id_ed25519.pub")
 }
 
 resource "aws_security_group" "test-sg" {
-  name        = "testing-sg"
+  name = "testing-sg"
   ingress {
     from_port   = 22
     to_port     = 22
